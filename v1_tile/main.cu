0fddbc04a59a35ac1dfd0da9c452ea256350b4a4
linton <sczlk@163.com>
2025-07-05 00:09:02 +0800

    basic cutlass+tiled

--- v1_tile/main.cu
+++ v1_tile/main.cu
@@ -6,6 +6,36 @@
 #include "common/check.h"
 #include <iostream>
 
+#define TILE_SIZE 16
+
+__global__ void matmul(Matrix A, Matrix B, Matrix C) {
+    __shared__ float tile_A[TILE_SIZE][TILE_SIZE];
+    __shared__ float tile_B[TILE_SIZE][TILE_SIZE];
+    __shared__ float tile_res[TILE_SIZE][TILE_SIZE];
+
+    float* matA = A.elements;
+    float* matB = B.elements;
+    float* matC = C.elements;
+
+    int tile_id_x = blockIdx.x;
+    int tile_id_y = blockIdx.y;
+    int tile_offset_x = threadIdx.x;
+    int tile_offset_y = threadIdx.y;
+    tile_res[tile_offset_x][tile_offset_y] = 0.0f;
+    for(int i=0;i<blockDim.x;i++){
+        tile_A[tile_offset_x][tile_offset_y] = matA[(i*TILE_SIZE+tile_offset_x)+A.width*(tile_id_y*TILE_SIZE+tile_offset_y)];
+        tile_B[tile_offset_x][tile_offset_y] = matB[(tile_id_x*TILE_SIZE+tile_offset_x)+B.width*(i*TILE_SIZE+tile_offset_y)];
+        __syncthreads();
+        float value = 0.0f;
+        for(int i=0;i<TILE_SIZE;i++) {
+            value += tile_A[tile_offset_x][i] * tile_B[i][tile_offset_y];
+        }
+        tile_res[tile_offset_x][tile_offset_y] += value;
+        __syncthreads();
+    }
+    matC[(tile_id_y*TILE_SIZE+tile_offset_y)*C.width + (tile_id_x*TILE_SIZE+tile_offset_x)] = tile_res[tile_offset_x][tile_offset_y];
+}
+
 int main(int argc,char* argv[]) {
     int dim =1024;
     if(argc>1) {
@@ -20,7 +50,9 @@ int main(int argc,char* argv[]) {
     Eval eval;
 
     float duration=eval.eval([&]() {
-        
+        dim3 grid(dim/TILE_SIZE, dim/TILE_SIZE);
+        dim3 block(TILE_SIZE, TILE_SIZE);
+        matmul<<<grid,block>>>(A,B,C);
     });
     std::cout<<"duration: "<<duration<<" ms"<<std::endl;
 }
--- vinfB_cutlass/main.cu
+++ vinfB_cutlass/main.cu
@@ -5,7 +5,17 @@
 #include "common/eval.h"
 #include "common/check.h"
 #include <iostream>
+#include "cutlass/gemm/device/gemm.h"
 
+
+using ColumnMajor = cutlass::layout::ColumnMajor;
+
+using CutlassGemm = cutlass::gemm::device::Gemm<float,        // Data-type of A matrix
+                                                  ColumnMajor,  // Layout of A matrix
+                                                  float,        // Data-type of B matrix
+                                                  ColumnMajor,  // Layout of B matrix
+                                                  float,        // Data-type of C matrix
+                                                  ColumnMajor>; // Layout of C matrix
 int main(int argc,char* argv[]) {
     int dim =1024;
     if(argc>1) {
@@ -20,7 +30,14 @@ int main(int argc,char* argv[]) {
     Eval eval;
 
     float duration=eval.eval([&]() {
-        
+        CutlassGemm::Arguments args({dim , dim, dim},  // Gemm Problem dimensions
+                              {A.elements, dim},    // Tensor-ref for source matrix A
+                              {B.elements, dim},    // Tensor-ref for source matrix B
+                              {C.elements, dim},    // Tensor-ref for source matrix C
+                              {C.elements, dim},    // Tensor-ref for destination matrix D (may be different memory than source C matrix)
+                              {1, 0});
+        CutlassGemm gemm_operator;
+        cutlass::Status status = gemm_operator(args);
     });
     std::cout<<"duration: "<<duration<<" ms"<<std::endl;
 }
